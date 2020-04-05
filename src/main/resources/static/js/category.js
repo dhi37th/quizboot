77be25b22b8131048109d592b36b197aa48f3d5d
Dhiraj Thakur <dhi37th@gmail.com>
2020-04-05 13:10:34 +0530

    Refactoring

--- src/main/resources/static/js/category.js
+++ src/main/resources/static/js/category.js
@@ -5,16 +5,16 @@ $(function(){
       "<div class='form-group form-row'>"+
         "<label for='categories["+newCat+"].name' class='col-sm-2 col-form-label'>Category</label>"+
         "<div class='col-sm-10'>"+
-          "<input required type='text' class='form-control input-sm catInput' id='categories"+newCat+".name' name='categories["+newCat+"].name' value=''>"+
+          "<input required type='text' class='form-control input-sm' id='categories"+newCat+".name' name='categories["+newCat+"].name' value='' oninput='changeInput(this);'>"+
         "</div>"+
       "</div>";
       $('#categoryDiv').append(text);
       $('#catSize').val(newCat + 1);
   });
-
-  $('.catInput').on('input' ,function(){
-      let p = this.selectionStart;
-      $(this).val($(this).val().toUpperCase());
-      this.setSelectionRange(p, p);
-  });
 });
+
+function changeInput(obj){
+   let p = obj.selectionStart;
+   $(obj).val($(obj).val().toUpperCase());
+   obj.setSelectionRange(p, p);
+}
